f0402f15ce55343ebb923929577478dcd587b3fa
dy <dyu32345@gmail.com>
2024-08-03 11:11:27 +0800

    利用阿里云脚手架搭建微服务初始程序

---
+++ src/main/java/com/dy/DyojBackendMicroserviseApplication.java
@@ -0,0 +1,13 @@
+package com.dy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyojBackendMicroserviseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DyojBackendMicroserviseApplication.class, args);
+    }
+
+}
